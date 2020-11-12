84acf4431269d7c2a11cbc56fffbcd9d4735b69a
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-11-12 18:28:51 -0500

    packager test

---
+++ packager/package.ps1
@@ -0,0 +1,6 @@
+$solpath = '..\solution_components'
+Import-Module ./Microsoft.Federal.PowerApps.SolutionGenerator.dll
+$xml = Resolve-SolutionGenerator $solpath
+New-Item -Path "../solution_components/other" -Name "solution.xml" -ItemType File -Force -Value $xml
+
+./SolutionPackager.exe /action:Pack /zipfile:../import/us1000_1_0_0_1.zip /folder:"..\solution_components"
