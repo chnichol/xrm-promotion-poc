053bec1c237bd3eabff4ef0af8e74b5b90f2e446
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-11-12 22:30:26 -0500

    solution import

--- packager/package.ps1
+++ packager/package.ps1
@@ -1,10 +1,17 @@
-Get-Host | Select-Object Version
+Install-Module Microsoft.Xrm.Data.PowerShell -Scope CurrentUser -Force
+
 Set-Location packager
 $solpath = '..\solution_components'
+
 Import-Module ./Microsoft.Federal.PowerApps.SolutionBuilder.dll
 $xml = Resolve-SolutionGenerator $solpath
-New-Item -Path "../solution_components/other" -Name "solution.xml" -ItemType File -Force -Value $xml
 
+New-Item -Path "../solution_components/other" -Name "solution.xml" -ItemType File -Force -Value $xml
 ./SolutionPackager.exe /action:Pack /zipfile:import.zip /folder:"..\solution_components"
+
+$currentLocation = Get-Location
+$solutionFileName = 'import.zip'
+
+Import-CrmSolution -conn $env:CRM_CONN -SolutionFilePath "$currentLocation\$solutionFileName" -PublishChanges -ActivatePlugIns -SkipDependancyOnProductUpdateCheckOnInstall
 
 Expand-Archive -Path import.Zip -DestinationPath ../artifacts/
