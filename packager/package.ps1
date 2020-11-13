483cfedf9b7fb9768ec06f064d876c35d80e6b0b
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-11-12 21:24:23 -0500

    unzip

--- packager/package.ps1
+++ packager/package.ps1
@@ -5,4 +5,6 @@ Import-Module ./Microsoft.Federal.PowerApps.SolutionBuilder.dll
 $xml = Resolve-SolutionGenerator $solpath
 New-Item -Path "../solution_components/other" -Name "solution.xml" -ItemType File -Force -Value $xml
 
-./SolutionPackager.exe /action:Pack /zipfile:../import/us1000_1_0_0_1.zip /folder:"..\solution_components"
+./SolutionPackager.exe /action:Pack /zipfile:import.zip /folder:"..\solution_components"
+
+Expand-Archive -Path import.Zip -DestinationPath ../import/
