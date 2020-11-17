a28d7049e62d276d61edd89d83daf5da9a0f27a4
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-11-17 00:04:40 -0500

    solutionGeneration

---
+++ packager/solutionGeneration.js
@@ -0,0 +1,18 @@
+
+const xml2js = require('xml2js');
+const fs = require('fs');
+const path = require('path');
+const parser = new xml2js.Parser({ async: false });
+
+const folderPath = '../solution_components';
+
+const component_filter = fs.readdirSync(folderPath)
+    .filter(f => f === 'Other')
+    .map(dir => fs.readdirSync(path.join(folderPath, dir))
+        .filter(f => f.match(/solution/i))
+        .map(f => fs.readFileSync(path.join(folderPath, dir, f), 'utf-8'))
+    );
+
+console.log(component_filter);
+const all_components = fs.readdirSync(folderPath)
+    .map(file => console.log(file));
