8e4694853b0136120fee3e3563fc58bd92eef8a1
Christopher Nichols <Christopher.Nichols3@va.gov>
2020-11-18 00:24:58 -0500

    solutionGeneration

--- packager/solutionGeneration.js
+++ packager/solutionGeneration.js
@@ -1,18 +1,35 @@
 
+const fse = require('fs-extra');
 const xml2js = require('xml2js');
 const fs = require('fs');
 const path = require('path');
 const parser = new xml2js.Parser({ async: false });
+<<<<<<< Updated upstream
 console.log(typeof process.env.TEST);
 const folderPath = '../solution_components';
+=======
+const folderPath = 'solution_components';
+const artifactsPath = 'artifacts';
+const all_comp = JSON.parse(process.env.all_comp);
+const spread = JSON.parse(process.env.SPREAD);
+>>>>>>> Stashed changes
 
-const component_filter = fs.readdirSync(folderPath)
-    .filter(f => f === 'Other')
-    .map(dir => fs.readdirSync(path.join(folderPath, dir))
-        .filter(f => f.match(/solution/i))
-        .map(f => fs.readFileSync(path.join(folderPath, dir, f), 'utf-8'))
-    );
+console.log(`Component folder: ${folderPath}`);
+console.log(`All changed files: ${process.env.all_comp}`);
 
-console.log(component_filter);
-const all_components = fs.readdirSync(folderPath)
-    .map(file => console.log(file));
+const component_filter = all_comp.filter(f=> f.match(folderPath));
+
+console.log(`solution_component changes: ${component_filter}`);
+console.log(`solution spread ${spread}`);
+
+fs.readdirSync(folderPath)
+  .filter((f) => {
+        if(!spread && f === "Other" || (f !== "Other" &&
+          component_filter.find((c) => c.match(`${folderPath}/${f}`))))
+          return f;
+
+  })
+  .map((f, i) => {
+    fse.copySync(`${folderPath}/${f}`, `${artifactsPath}/_${spread && i || 0}/${f}`);
+    spread && fse.copySync(`${folderPath}/Other`, `${artifactsPath}/_${i}/Other`);
+  });
