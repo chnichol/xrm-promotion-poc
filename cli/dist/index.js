0dd3b7cbd42b05031a4460a8d978837cf8330fb5
Xisrith <xisrith@gmail.com>
2021-07-21 16:12:42 -0500

    Removed cli dist from tracking

--- cli/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var cli_1 = __importDefault(require("./components/cli"));
-//const argv = process.argv.slice(2);
-cli_1.default(process.argv.slice(2));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSx5REFBbUM7QUFFbkMscUNBQXFDO0FBQ3JDLGFBQUcsQ0FBQyxPQUFPLENBQUMsSUFBSSxDQUFDLEtBQUssQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDIn0=
