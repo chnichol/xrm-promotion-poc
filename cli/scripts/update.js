80b77059deac2bfa3f96cb3b35071a6e875765cd
Xisrith <xisrith@gmail.com>
2021-07-26 17:38:35 -0500

    Added a script for installing/updating the xrm-cli utility

---
+++ cli/scripts/update.js
@@ -0,0 +1,30 @@
+const { exec } = require('child_process');
+const fs = require('fs/promises');
+const path = require('path');
+
+const execPromise = (command) => {
+    const child = exec(command);
+    const promise = new Promise((resolve, reject) => {
+        child.addListener('error', reject);
+        child.addListener('exit', resolve);
+    });
+    child.promise = promise;
+    return child;
+}
+
+const update = async () => {
+    await execPromise('npm install -g .').promise;
+    if (process.platform === 'win32') {
+        const dir = path.join(process.env.HOME, 'AppData', 'Roaming', 'npm');
+        const ps1 = path.join(dir, 'xrm-cli.ps1');
+        const cmd = path.join(dir, 'xrm-cli.cmd');
+
+        await fs.rm(ps1);
+        
+        const script = await fs.readFile(cmd, 'ascii');
+        const rewrite = script.replace(/"%dp0%\\node_modules\\xrm-cli\\dist\\index.js"/g, 'node "%dp0%\\node_modules\\xrm-cli\\dist\\index.js"');
+        await fs.writeFile(cmd, rewrite);
+    }
+}
+
+update();
