971b96a6b50d5ee2c48e94e154e5e1e5d7f46c00
Xisrith <xisrith@gmail.com>
2021-08-04 17:27:18 -0500

    Added a semaphore to the getToken method to prevent collisions

--- cli/src/auth/getToken.ts
+++ cli/src/auth/getToken.ts
@@ -1,8 +1,13 @@
 import { AuthenticationResult } from '@azure/msal-node';
+import { Lock } from 'semaphore-async-await';
 import createToken from './createToken';
 import TokenStore from './tokenStore';
 
+const lock = new Lock();
+
 export default async (): Promise<AuthenticationResult> => {
+    await lock.acquire();
+
     const store = new TokenStore('.xrm/id');
     const idToken = await store.loadToken();
     if (idToken && idToken.expiresOn && new Date(idToken.expiresOn) > new Date()) {
@@ -12,5 +17,7 @@ export default async (): Promise<AuthenticationResult> => {
     // TODO: Enable using refresh tokens when the app registration has trust.
     const newToken = await createToken();
     await store.saveToken(newToken);
+
+    lock.release();
     return newToken;
 };
