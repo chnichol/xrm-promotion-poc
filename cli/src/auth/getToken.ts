ac9cf8bbda0a7d57f142952f30b00ff54dec3c24
James Darling <jamesdarling@microsoft.com>
2021-08-05 12:30:27 -0500

    Moved semaphore lock to only affect login

--- cli/src/auth/getToken.ts
+++ cli/src/auth/getToken.ts
@@ -6,18 +6,17 @@ import TokenStore from './tokenStore';
 const lock = new Lock();
 
 export default async (): Promise<AuthenticationResult> => {
-    await lock.acquire();
-
     const store = new TokenStore('.xrm/id');
     const idToken = await store.loadToken();
     if (idToken && idToken.expiresOn && new Date(idToken.expiresOn) > new Date()) {
         return idToken;
     }
-
+    
     // TODO: Enable using refresh tokens when the app registration has trust.
+    await lock.acquire();
     const newToken = await createToken();
     await store.saveToken(newToken);
-
     lock.release();
+
     return newToken;
 };
