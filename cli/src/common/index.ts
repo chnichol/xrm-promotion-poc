1f7f997e084bf4961cb06f2d201ece2c1f23c020
James Darling <jamesdarling@microsoft.com>
2021-08-16 17:25:45 -0500

    Moving isolated methods from common to helpers in preperation for implementing unit testing.

--- cli/src/common/index.ts
+++ cli/src/common/index.ts
@@ -10,17 +10,6 @@ export interface ChildProcessWithPromise extends ChildProcess {
     promise: Promise<unknown>;
 }
 
-export const createSearchUrl = (url: string, parameters: { [key: string]: string | undefined }, base?: string | URL | undefined): URL => {
-    const searchUrl = new URL(url, base);
-    for (const key in parameters) {
-        const value = parameters[key];
-        if (value) {
-            searchUrl.searchParams.append(key, value);
-        }
-    }
-    return searchUrl;
-}
-
 export const execPromise = (command: string): ChildProcessWithPromise => {
     const child = exec(command) as ChildProcessWithPromise;
     const promise = new Promise((resolve, reject) => {
@@ -42,13 +31,6 @@ export const exists = async (path: string): Promise<boolean> => {
         }
         throw e;
     }
-}
-
-export const getPositionals = (args: { _: (number | string)[] }): string[] => args._.slice(1).map(a => a.toString());
-
-export const isUuid = (text: string): boolean => {
-    const uuidRegex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/g;
-    return (text.match(uuidRegex) ?? []).length > 0
 }
 
 export const mkdir = (path: string): Promise<string | void | undefined> => fs.mkdir(path, { recursive: true })
---
+++ cli/src/helpers/getPositionals.ts
@@ -0,0 +1,4 @@
+const getPositionals = (args: { _: (number | string)[] }): string[] => args._.slice(1).map(a => a.toString());
+
+
+export default getPositionals;
